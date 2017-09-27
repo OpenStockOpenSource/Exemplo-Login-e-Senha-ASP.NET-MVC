9578d52f6bb52140fe83e3528819929c88ad265b
Luiz Guilherme Bandeira <arkanael@gmail.com>
2017-09-27 11:14:30 -0300

    Exemplo Login e Senha ASP.NET MVC
    Exemplo teste de login utilizando ASPNET MVC, EntityFrameWork, SQL LocalDB, Criptografia MD5

---
+++ Projeto.DAL/Mappings/AprovacaoPedidosMAP.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration;
+using Projeto.Entities;
+
+namespace Projeto.DAL.Mappings
+{
+    public class AprovacaoPedidosMAP:EntityTypeConfiguration<AprovacaoPedidos>
+    {
+        public AprovacaoPedidosMAP()
+        {
+            ToTable("AprovacaoPedidos");
+            HasKey(a => a.Id);
+
+            Property(a => a.Id)
+                .HasColumnName("Id");
+
+            Property(a => a.NumeroOFProducao)
+                .HasColumnName("NumeroOfProducao")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            Property(a => a.DataFabricacao)
+                .HasColumnName("DataFabricacao")
+                .IsRequired();
+
+            Property(a => a.DataAnalise)
+                .HasColumnName("DataAnalise")
+                .IsRequired();
+
+            Property(a => a.NumeroCertificado)
+                .HasColumnName("NumeroCertificado")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            Property(a => a.NumeroOFLaboratorio)
+                .HasColumnName("NumeroOFLaboratorio")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            Property(a => a.NomeProduto)
+                .HasColumnName("NomeProduto")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            Property(a => a.QuantidadeKG)
+                .HasColumnName("QuantidadeKG")
+                .HasMaxLength(10)
+                .IsRequired();
+
+            Property(a => a.Situacao)
+                .HasColumnName("Situacao")
+                .HasMaxLength(50);
+
+            Property(a => a.AtualizadoPor)
+                .HasColumnName("AtualizadoPor")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            Property(a => a.Observacao)
+                .HasColumnName("Observacao")
+                .HasMaxLength(100);
+        }
+    }
+}
---
+++ Projeto.Entities/AprovacaoPedidos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Entities
+{
+    public class AprovacaoPedidos
+    {
+        public int Id { get; set; }
+        public string NumeroOFProducao { get; set; }
+        public DateTime DataFabricacao { get; set; }
+        public DateTime DataAnalise { get; set; }
+        public string NumeroCertificado { get; set; }
+        public string NumeroOFLaboratorio { get; set; }
+        public string NomeProduto { get; set; }
+        public string QuantidadeKG { get; set; }
+        public String Situacao { get; set; }
+        public string AtualizadoPor { get; set; }
+        public string Observacao { get; set; }
+    }
+}
